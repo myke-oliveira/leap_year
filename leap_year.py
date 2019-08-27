e840de275ca75da11144fa37a259ffd2d0efd282
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-08-27 09:40:43 -0300

    is_leap_year funcionality tests done

---
+++ leap_year.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 09:29:35 2019
+
+@author: myke
+"""
+
---
+++ tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from leap_year import is_leap_year
+
+
+
+
+
+
