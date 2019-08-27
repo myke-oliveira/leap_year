d543aa79bee23223945c11b803f846e63b3b470e
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-08-27 13:50:03 -0300

    Web Funcionality available.

--- run_flask.py
+++ run_flask.py
@@ -6,14 +6,17 @@ Created on Tue Aug 27 10:09:09 2019
 @author: myke
 """
 
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+from leap_year import is_leap_year
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-@app.route('/home')
-def index():
-    return 'Hello, World!'
+@app.route('/', methods = ['POST', 'GET'])
+def index(result=None):
+    print(request.method)
+    if request.method == 'POST':
+        year = int(request.form['year'])
+        result = f'{year}: {"Leap Year" if is_leap_year(year) else "Not a Leap Year"}'
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
