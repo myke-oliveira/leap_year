979937372217fd4534f944ebb1d52da221dbaee4
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2019-08-27 09:50:07 -0300

    is_leap_year funcionality working

--- leap_year.py
+++ leap_year.py
@@ -6,3 +6,4 @@ Created on Tue Aug 27 09:29:35 2019
 @author: myke
 """
 
+is_leap_year = lambda year: (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
--- tests.py
+++ tests.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*-
 from leap_year import is_leap_year
 
+assert is_leap_year(1600) == True
+assert is_leap_year(1732) == True
+assert is_leap_year(1888) == True
+assert is_leap_year(1944) == True
+assert is_leap_year(2008) == True
 
-
-
-
-
+assert is_leap_year(1742) == False
+assert is_leap_year(1889) == False
+assert is_leap_year(1951) == False
+assert is_leap_year(2011) == False
+assert is_leap_year(1700) == False
